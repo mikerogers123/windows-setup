fd60c7d9a15bc61fe31144f26f2ee4688ece1207
Mike Rogers <mikerogers_@hotmail.co.uk>
2020-08-06 21:00:16 +0100

    Initial setup script

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install git
+choco install poshgit
+choco install nodejs
+refreshenv
+npm install
+npm install -g @angular/cli
